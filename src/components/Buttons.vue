7a02304d67bffb48e20707ec8f78a0c0fe927284
juanesadriano <juaneshtk50>
2017-09-23 11:58:45 -0300

    Começando a definir os métodos pros AJAX

--- src/components/Buttons.vue
+++ src/components/Buttons.vue
@@ -1,12 +1,31 @@
 <template>
-<div>
-   <a class="waves-effect waves-light btn">Sou um botão</a>
-    <a class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>outro botão</a>
-</div>
+    <div>
+        <a @click="callUsers" class="waves-effect waves-light btn">Chamar usuários</a>
+        <a @click="countUsers" class="waves-effect waves-light btn">Contar usuários</a>
+        <a class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>outro botão</a>
+
+        <hr/>
+        <pre>
+        {{users | json}}
+    </pre>
+    </div>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                users: null
+            }
+        }
 
+        methods: {
+            callUsers: function () {
+                this.$http({url: '/users.json', method: 'GET'}).
+                then(function (response) {
+
+                })
+            }
+        }
     }
 </script>

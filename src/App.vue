ff5fbf232720a3809283d69c9e7429c5a75ad506
juanesadriano <juaneshtk50>
2017-09-23 09:10:11 -0300

    Fechando exemplo de Eventos e Emit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="app">
+        <my-menu title="My App" v-on:button-click="onBtClick">
+            <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+            </ul>
+
+        </my-menu>
+        <h1>{{msg}}</h1>
+    </div>
+</template>
+
+<script>
+    import myMenu from './myMenu.vue'
+
+    export default {
+
+
+        data() {
+            return {
+                msg: 'VueVueVue'
+            }
+        },
+        components: {
+            myMenu
+        },
+        methods:{
+           onMenuClick:function (e) {
+               alert("Clicou no menu!");
+           },
+            onBtClick:function (message) {
+                alert(message);
+
+            }
+
+        }
+    }
+</script>
+
+<style>
+    body {
+        font-family: Arial, Helvetica, sans-serif;
+    }
+</style>
---
+++ src/myMenu.vue
@@ -0,0 +1,35 @@
+
+<template>
+    <div >
+        <h4>{{title}}</h4><br>
+        <button @click="onBtClick">EU?</button>
+        <div>
+            <slot></slot>
+        </div>
+    </div>
+
+</template>
+
+
+<script>
+    export default {
+        props:{
+            propNumb: Number,
+            title: Text,
+
+        },
+        methods:{
+            onMenuClick: function () {
+                this.$emit('menu-click');
+            },
+            onBtClick: function () {
+                this.$emit('button-click', "ta comigo ta com deus", "ta lgd")
+            }
+        }
+
+    }
+</script>
+
+<style>
+
+</style>
